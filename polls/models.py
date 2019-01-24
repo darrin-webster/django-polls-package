abc18e88249d08e166a02117cae718511f5fdc2a
Darrin Webster <dw@hf.cx>
2019-01-23 21:02:18 -0800

    Polls application used for voting in Django tutorial project.

---
+++ polls/models.py
@@ -0,0 +1,37 @@
+# im port models od django database
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# class qeustion
+class Question(models.Model):
+    #form field for question; a character field not to exceed 200 chars
+    question_text = models.CharField(max_length=200)
+    # Logs date when published
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
+
+    
+# class choice
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+
+# The code is straightforward. Each model is represented by a class that subclasses django.db.models.Model. 
+# Each model has a number of class variables, each of which represents a database field in the model.
+
+# Each field is represented by an instance of a Field class – e.g., CharField for character fields and 
+# DateTimeField for datetimes. This tells Django what type of data each field holds.
